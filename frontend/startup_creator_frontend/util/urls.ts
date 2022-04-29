6ac280d0b5e46631b4de813f60a57ff12ee86e09
lucks <takuto.yoshikai@gmail.com>
2022-04-29 15:43:12 +0900

    urls

---
+++ frontend/startup_creator_frontend/util/urls.ts
@@ -0,0 +1,13 @@
+
+
+function openDomainShop(domainId: string): void {
+  window.open("https://muumuu-domain.com/domain/search?domain=" + domainId);
+}
+
+function openBCGenerator(): void {
+  window.open("https://takutoyoshikai.github.io/bc-generator");
+}
+
+export openDomainShop;
+export openBCGenerator;
+
